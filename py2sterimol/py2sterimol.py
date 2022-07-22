523c26c432df665a9b05a6b2a23baa36c83be09e
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-07-21 22:30:09 -0400

    add basic layout

--- py2sterimol/py2sterimol.py
+++ py2sterimol/py2sterimol.py
@@ -1,3 +1,11 @@
 
-def placeholder():
-    return
+class py2sterimol:
+    def __init__(
+        self,
+        smiles,
+        xyz,
+    ) -> None:
+        self._smiles = smiles
+        self._xyz = xyz
+
+    
---
+++ py2sterimol/utils/tools.py
@@ -0,0 +1,11 @@
+def smiles_to_verloop(smiles: str) -> str:
+    """
+    Take a smiles string and return the equivalent Verloop-encoded string.
+
+    Args:
+        smiles (str): Input SMILES string
+
+    Returns:
+        str: Verloop-encoded string.
+    """
+    pass
